ceebce6c2ba1cbbca9ed99286171cb642eb692be
Oluwasemilore02 <semioreatitebi@gmail.com>
2025-05-02 11:18:20 +0100

    Add files via upload

---
+++ JAVASCRIPT 4/script.js
@@ -0,0 +1,18 @@
+// This is the first Value
+let bill = 275
+let tip = bill / 100 * 15
+let total = bill + tip
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${total}`)
+
+// This is the second Value
+let bill2 = 40
+let tip2 = bill2 / 100 * 20
+let total2 = bill2 + tip2
+console.log(`The bill was ${bill2}, the tip was ${tip2}, and the total value is ${total2}`)
+
+// This is the third Value
+let bill3 = 430
+let tip3 = bill3 / 100 * 20
+let total3 = bill3 + tip3
+console.log(`The bill was ${bill3}, the tip was ${tip3}, and the total value is ${total3}`)
+
